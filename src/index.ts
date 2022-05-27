0aaa2195ceac7c5ebd1b5c24f5fa528a70217731
danjerezTdf <104039810+danjerezTdf@users.noreply.github.com>
2022-05-27 15:25:44 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
+/* Múltiplos
+• Cree un algoritmo que visualice los
+números que son múltiplos de 2 o
+de 3 que hay entre 1 y 100
+• Tener en cuenta que hay números
+que son múltiplos de 2 y de 3 al
+mismo tiempo
+• En dichos casos, solamente
+indique el número una vez */
+
 let contador: number = 0;
 
 while (contador <= 100) {
